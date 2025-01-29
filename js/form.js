80cd68818d44d6bfb9d20a8f49983d16e5e257c6
yash1hingu <yash1hingu@gmail.com>
2025-01-29 18:41:52 +0530

    UPDATE: Form validation

--- js/form.js
+++ js/form.js
@@ -3,124 +3,197 @@ $(document).ready(function () {
         $("#myForm").submit(function (event) {
             event.preventDefault();
     
-            const file = $("#file")[0].files[0];
-            const color = $("#color").val();
-            const date = $("#date").val();
-            const time = $("#time").val();
-            const month = $("#month").val();
-            const email = $("#email").val();
-            const url = $("#url").val();
-            const nourl = $("#nourl").val();
-            const number = $("#number").val();
-            const rangenumber = $("#rangenumber").val();
-            const telenumber = $("#telenumber").val();
-            const password = $("#password").val();
-            const confirmpassword = $("#confirmpassword").val();
-            const rangetext = $("#rangetext").val();
-            const select = $("#select").val();
+            const file = $("#file")[0];
+            const color = $("#color");
+            const date = $("#date");
+            const time = $("#time");
+            const month = $("#month");
+            const email = $("#email");
+            const url = $("#url");
+            const nourl = $("#nourl");
+            const number = $("#number");
+            const rangenumber = $("#rangenumber");
+            const telenumber = $("#telenumber");
+            const password = $("#password");
+            const confirmpassword = $("#confirmpassword");
+            const rangetext = $("#rangetext");
+            const select = $("#select");
             const radioyes = $("#radioyes:checked").length;
             const radiono = $("#radiono:checked").length;
             const checkboxes = $("#checkboxes:checked").length;
-    
-            if(!file){
-                alert("File Should be containe");
+            
+            $("input").removeClass("warning");
+            $("select").removeClass("warning");
+            $("p.warning").remove();
+            
+            if(!file.files[0]){
+                $(file).addClass("warning");
+                $(file).after(`<p class="warning">Please fill out this filed.</p>`);
+                $('#main-content').animate({
+                    scrollTop: ($(file).offset().top)
+                }, 1000);;
                 return;
             }
     
-            if(color.trim() === ""){
-                alert("Color Should be Selected.");
+            if(color.val().trim() === ""){
+                $(color).addClass("warning");
+                $(color).after(`<p class="warning">Please fill out this filed.</p>`);
+                $('#main-content').animate({
+                    scrollTop: ($(color).offset().top)
+                }, 1000);;
                 return;
             }
     
     
-            if(date === ""){
-                alert("date should be selected.");
+            if(date.val() === ""){
+                $(date).addClass("warning");
+                $(date).after(`<p class="warning">Please fill out this filed.</p>`);
+                $('#main-content').animate({
+                    scrollTop: ($(date).offset().top)
+                }, 1000);;
                 return;
             }
     
-            if(time === ""){
-                alert("Time should be selected.");
+            if(time.val() === ""){
+                $(time).addClass("warning");
+                $(time).after(`<p class="warning">Please fill out this filed.</p>`);
+                $('#main-content').animate({
+                    scrollTop: ($(time).offset().top)
+                }, 1000);;
                 return;
             }
     
-            if(month === ""){
-                alert("Month Should be selected.");
+            if(month.val() === ""){
+                $(month).addClass("warning");
+                $(month).after(`<p class="warning">Please fill out this filed.</p>`);
+                console.log(-$(month).offset().top)
+                $('#main-content').animate({
+                    scrollTop: ($(month).offset().top)
+                }, 1000);
                 return;
             }
     
     
             const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/;
             // Validate email format
-            if (!emailPattern.test(email)) {
-                alert("Please enter a valid email address.");
+            if (!emailPattern.test(email.val())) {
+                $(email).addClass("warning");
+                $(email).after(`<p class="warning">Please fill out this filed.</p>`);
+                $('#main-content').animate({
+                    scrollTop: ($(email).offset().top)
+                }, 1000);
                 return;
             }
     
-            if(url === "" || url.trim() === ""){
-                alert("Url should be Enter.");
+            if(url.val() === "" || url.val().trim() === ""){
+                $(url).addClass("warning");
+                $(url).after(`<p class="warning">Please fill out this filed.</p>`);
+                $('#main-content').animate({
+                    scrollTop: ($(url).offset().top)
+                }, 1000);
                 return;
             }
     
-            if(nourl === "" || nourl.trim() === ""){
-                alert("Url without TLD Should be not Empty.");
+            if(nourl.val() === "" || nourl.val().trim() === ""){
+                $(nourl).addClass("warning");
+                $(nourl).after(`<p class="warning">Please fill out this filed.</p>`);
+                $('#main-content').animate({
+                    scrollTop: ($(nourl).offset().top)
+                }, 1000);
                 return;
             }
     
-            if(number === ""){
-                alert("Number Should not be Empty.");
+            if(number.val() === ""){
+                $(number).addClass("warning");
+                $(number).after(`<p class="warning">Please fill out this filed.</p>`);
+                $('#main-content').animate({
+                    scrollTop: ($(number).offset().top)
+                }, 1000);
                 return;
             }
     
-            if(rangenumber === ""){
-                alert("Range Number Should be not Empty.");
+            if(rangenumber.val() === ""){
+                $(rangenumber).addClass("warning");
+                $(rangenumber).after(`<p class="warning">Please fill out this filed.</p>`);
+                $('#main-content').animate({
+                    scrollTop: ($(rangenumber).offset().top)
+                }, 1000);
                 return;
             }
     
             const telPattern = /^[0-9]{10}$/;
-            if(telenumber === ""){
-                alert("Telephonic number should not be Empty.");
+            if(telenumber.val() === ""){
+                $(telenumber).addClass("warning");
+                $(telenumber).after(`<p class="warning">Please fill out this filed.</p>`);
+                $('#main-content').animate({
+                    scrollTop: ($(telenumber).offset().top)
+                }, 1000);
                 return;
             }
-            if(!telPattern.test(telenumber)){
-                alert("Telephonic should be in valid formate.");
+            if(!telPattern.test(telenumber.val())){
+                $(telenumber).addClass("warning");
+                $(telenumber).after(`<p class="warning">Please fill out in valid formate.</p>`);
+                $('#main-content').animate({
+                    scrollTop: ($(telenumber).offset().top)
+                }, 1000);
                 return;
             }
     
             // Validate password length (at least 8 characters)
-            if (password.trim() === "" || password.length < 8) {
-                alert(
-                    "Password must be at least 8 characters and no spaces."
-                );
+            if (password.val().trim() === "" || password.val().length < 8) {
+                $(password).addClass("warning");
+                $(password).after(`<p class="warning">Password must be at least 8 characters and no spaces.</p>`);
+                $('#main-content').animate({
+                    scrollTop: ($(password).offset().top)
+                }, 1000);
                 return;
             }
     
             // Validate password match
-            if (password !== confirmpassword) {
-                alert("Passwords do not match.");
+            if (password.val() !== confirmpassword.val()) {
+                $(confirmpassword).addClass("warning");
+                $(confirmpassword).after(`<p class="warning">Passwords do not match.</p>`);
+                $('#main-content').animate({
+                    scrollTop: ($(password).offset().top)
+                }, 1000);
                 return;
             }
     
-            if(rangetext === ""){
-                alert("Text with Minimum and Maximum Length should satisfy.");
+            if(rangetext.val() === ""){
+                $(rangetext).addClass("warning");
+                $(rangetext).after(`<p class="warning">Text with Minimum and Maximum Length should satisfy.</p>`);
+                $('#main-content').animate({
+                    scrollTop: ($(rangetext).offset().top)
+                }, 1000);
                 return;
             }
     
-            if(!(rangetext.length >= 3 & rangetext.length <=9)) {
-                alert("Text should be in range.")
+            if(!(rangetext.val().length >= 3 & rangetext.val().length <=9)) {
+                $(rangetext).addClass("warning");
+                $(rangetext).after(`<p class="warning">Text should be in range.</p>`);
+                $('#main-content').animate({
+                    scrollTop: (-$(rangetext).offset().top)
+                }, 1000);
                 return;
             }
     
-            if (select === "select") {
-                alert("Select should be Selected.");
+            if (select.val() === "select") {
+                $(select).addClass("warning");
+                $(select).after(`<p class="warning">Select should be Selected.</p>`);
+                $('#main-content').animate({
+                    scrollTop: ($(select).offset().top)
+                }, 1000);
                 return;
             }
     
             if(radiono === radioyes){
-                alert("Radio Button should be selected YES or NO.");
+                $(radiono).addClass("warning");
+                $(radiono).after(`<p class="warning">Radio Button should be selected YES or NO.</p>`);
                 return;
             }
             if(checkboxes === 0){
-                alert("Checkbox should be tick.");
+                $(checkboxes).addClass("warning");
+                $(checkboxes).after(`<p class="warning">Checkbox should be tick.</p>`);
                 return;
             }
     
